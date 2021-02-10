14f08c1b25c7ef21f090e39deb07fb347028309a
Laurine Adhiambo <laurinealuoch7@gmail.com>
2021-02-10 10:12:20 +0300

    for loops

---
+++ Iterations/for/for_loop.py
@@ -0,0 +1,8 @@
+__author__ = "Laurine"
+
+# Specifies the numbre of times to run the code block
+for i in range(0, 100):
+
+    # the code_block to be executed in the range
+    if i % 2 == 0:
+        print(i)
